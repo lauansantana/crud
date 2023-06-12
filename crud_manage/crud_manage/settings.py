8fe4d4a0e065cd8226c60d0b36fe2962c5699c22
Lauan Santana <lauestora2@gmail.com>
2023-06-12 20:39:06 -0300

    deploy

--- crud_manage/crud_manage/settings.py
+++ crud_manage/crud_manage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i#5%%p90s-5^&kzrsnrjuck#ada1xpib&!#jp%-gg%jsg_h@0d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
